34501f706f4e5f27f7a9cff62d4a9c4501f1c9a0
Ryan Gundu <rgundu@mail.uoguelph.ca>
2018-10-01 08:22:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var app	= express();
+var app = express();
 var path = require('path');
 var request = require('request');
 var bodyParser = require('body-parser');
